7ff8bbdae4c10c6134a138dddd4e52c080c733a6
MonishDoss <monishdoss2005@gmail.com>
2025-08-09 19:22:58 +0530

    Added post card

---
+++ instagram-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import Post from "./compount/Post/Post"
+
+function App() {
+  return (
+    <>
+    <Post
+    profilePhoto="https://static.vecteezy.com/system/resources/previews/019/900/322/non_2x/happy-young-cute-illustration-face-profile-png.png"
+    name="TestingName"
+    time='.2h'
+    />
+    </>
+  )
+}
+
+export default App
